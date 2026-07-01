10f2d4df2bfa99d6c7a062fe906bdbec8d7faaa3
Suraj Kumar <surajkumar44727@gmail.com>
2026-07-01 11:08:55 +0530

    chore: add provider for google and github

--- models/User.ts
+++ models/User.ts
@@ -3,7 +3,9 @@ import bcrypt from "bcryptjs";
 
 export interface IUser {
     email: string;
-    password: string;
+    password?: string;
+    provider: "credentials" | "google" | "github";
+    image?: string;
     _id?: Schema.Types.ObjectId;
     createdAt?: Date;
     updatedAt?: Date;
@@ -18,16 +20,24 @@ const UserSchema = new Schema<IUser>(
         },
         password: {
             type: String,
-            required: true,
+        },
+        provider: {
+            type: String,
+            enum: ['credentials', 'google', 'github'],
+            default: 'credentials',
+        },
+        image: {
+            type: String,
         },
     },
     {timestamps: true}
 );
 
 UserSchema.pre('save', async function () {
-    if(this.isModified('password')){
-        this.password = await bcrypt.hash(this.password, 10)
+    if( !this.password || !this.isModified('password')){
+        return;
     }
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = models?.User || model<IUser>("User", UserSchema)
